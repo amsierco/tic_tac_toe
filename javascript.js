3a7e1a1ac5585365fa76d03ac8780d7374b259d4
Austin <75064473+amsierco@users.noreply.github.com>
2023-02-25 21:26:18 -0500

    UI and 3x board space

--- javascript.js
+++ javascript.js
@@ -12,14 +12,17 @@ const boardFactory = board => {
     const getNumber = () => board.getAttribute('class').substr(7);
     const getId = () => board.getAttribute('id');
     const getBoard = () => board;
-
+    const getChildren = () => children;
     const children = Array.from(board.children);
+
     children.forEach(btn => {
         btn.addEventListener('click', () => {
-            gameManager.takeTurn(btn);
-            // gameConditions.checkForWin(board);
-            gameManager.checkConditions(getNumber());
-            //console.log(board.getAttribute('class').substr(6));
+            if(board.getAttribute('id') == 'in-progress' &&
+               btn.getAttribute('id') == 'unmarked'){
+                btn.setAttribute('id', 'marked');
+                gameManager.takeTurn(btn);
+                gameManager.checkConditions(getNumber());
+            }
         });
     });
 
@@ -27,7 +30,7 @@ const boardFactory = board => {
         return (document.querySelector('.b'+board.getAttribute('class').substr(7)+' .c'+column+row)); 
     }
 
-    const winCases = [
+    const localWinCases = [
         //Columns
         [getCell(0,0),getCell(0,1),getCell(0,2)],
         [getCell(1,0),getCell(1,1),getCell(1,2)],
@@ -39,14 +42,9 @@ const boardFactory = board => {
         //Diagonal
         [getCell(0,0),getCell(1,1),getCell(2,2)],
         [getCell(0,2),getCell(1,1),getCell(2,0)],
-    ];
+    ]; 
 
-//console.log(winCases);
-
-    const winPatterns = () => winCases;
-    
-
-    return {getNumber, getBoard, getId, winPatterns, winCases};
+    return {getNumber, getBoard, getId, getChildren, localWinCases};
 };
 
 
@@ -58,62 +56,37 @@ const gameManager = (() => {
     const playerStorage = [playerOne, playerTwo];
     var order = 0;
     const boardStorage = [];
-    const test=[];
 
-
-    //Array of board objects
+    //Create an array of game boards
     for (let i=0; i<document.querySelectorAll('.board').length; i++){
         boardStorage[i] = boardFactory(document.querySelector('.b'+i));
-        test[i]  =document.querySelector('.b'+i);
     }
 
-    for (let index = 0; index < test.length; index++){
-        //console.log(test[index]);
-        //console.log(boardStorage[index].getBoard());
-      
-    }
-
+    //Add's player's symbol to board
     function takeTurn(btn) {
-        if(btn.getAttribute('id') == 'marked'){
-            return false;
-        }
-        else
-        {
-            btn.setAttribute('id', 'marked');
-        }
         btn.textContent = playerStorage[order].getSymbol();
         order >= playerStorage.length-1 ? order=0 : order++;
-        //checkGameState();
     }
 
+    //Checks conditions on specific boards based board button inputs
     function checkConditions(boardId){
         gameConditions.checkForWin(boardStorage[boardId]);
+        gameConditions.bigWin();
     }
-
 
     return {takeTurn, checkConditions};
 })();
 
 const gameConditions = (() => {
 
+    //Checks for individual board victories
     function checkForWin(board){
-       // console.log(board.getBoard().getAttribute('class'));
-        //const children = board.getBoard().querySelectorAll('.cell');//document.querySelectorAll('.b'+board.getNumber());
-        //console.log(children);
-        for(let i=0; i<board.winCases.length; i++){
-            if(allEqual(board.winCases[i])){
-                board.getBoard().style.backgroundColor = 'purple';
-            }
-        }
-    }
-
-    function checkGameState(){
-        for(let i=0; i<boardStorage.length; i++){
-            if(boardStorage[i].getId() == 'in-progress' && checkForWin(boardStorage[i].getBoard()) == true){
-                // console.log('class: '+boardStorage[i].getClass());
-                boardStorage[i].getBoard().style.backgroundColor = 'blue';
-                // console.log(boardStorage[i].getClass());
-                boardStorage[i].getBoard().setAttribute('id', 'done');
+        for(let i=0; i<board.localWinCases.length; i++){
+            if(allEqual(board.localWinCases[i])){
+                console.log(board.getBoard().getAttribute('class').substr(6)+' has completed!');
+                board.getBoard().setAttribute('id', 'done'+board.localWinCases[i][0].textContent);
+                makeBig(board, board.localWinCases[i][0].textContent);
+                return true;
             }
         }
     }
@@ -125,6 +98,66 @@ const gameConditions = (() => {
         return (arr[0].textContent == arr[1].textContent && arr[1].textContent == arr[2].textContent);
     }
 
-    return {checkForWin, checkGameState};
+    function makeBig(board, symbol){
+        if(symbol == 'O'){
+            board.getChildren()[4].style.background = 'transparent';
+        } else if (symbol == 'X'){
+            board.getChildren()[1].style.background = 'transparent';
+            board.getChildren()[3].style.background = 'transparent';
+            board.getChildren()[5].style.background = 'transparent';
+            board.getChildren()[7].style.background = 'transparent';
+        }
+
+        for(let child of board.getChildren()){
+            child.textContent = '';
+        }
+    }
+
+    function allEqualLarge (arr) {
+        if(arr[0] == 'flase' || arr[1] == 'false' || arr[2] == 'false'){
+            return false;
+        }
+        else if(arr[0].getAttribute('id').substr(4) == 'false' || arr[1].getAttribute('id').substr(4) == 'false' || arr[2].getAttribute('id').substr(4) == 'false'){
+            return false;
+        } else {
+            arr[0].style.backgroundColor = 'rgba(100,255,100,1);';
+            arr[1].style.backgroundColor = 'rgba(100,255,100,1);';
+            arr[2].style.backgroundColor = 'rgba(100,255,100,1);';
+            return (arr[0].getAttribute('id').substr(4) == arr[1].getAttribute('id').substr(4) && arr[1].getAttribute('id').substr(4) == arr[2].getAttribute('id').substr(4));
+        }
+    }
+
+    function getCell (b){
+        if(document.querySelector('.b'+b).getAttribute('id') == 'in-progress'){
+            return 'false';
+        }
+        return (document.querySelector('.b'+b)); 
+    }
+
+    function bigWin(){
+        let globalWinCases = [
+            //Columns
+            [getCell(0),getCell(3),getCell(6)],
+            [getCell(1),getCell(4),getCell(7)],
+            [getCell(2),getCell(5),getCell(8)],
+            //Rows
+            [getCell(0),getCell(1),getCell(2)],
+            [getCell(3),getCell(4),getCell(5)],
+            [getCell(6),getCell(7),getCell(8)],
+            //Diagonal
+            [getCell(0),getCell(4),getCell(8)],
+            [getCell(6),getCell(4),getCell(2)],
+        ]; 
+
+
+        for(let i=0; i<globalWinCases.length; i++){
+            if(allEqualLarge(globalWinCases[i]) == true){
+                console.log("Mega Winner: "+globalWinCases[i][0].getAttribute('id').substr(4));
+                return true;
+            }
+        }
+    }
+
+    return {checkForWin, bigWin};
 
 })();
